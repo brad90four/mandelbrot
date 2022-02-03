7067265089fcf815af2bb67d3152c2695f183a62
brad90four <42116429+brad90four@users.noreply.github.com>
2022-02-03 15:17:52 -0500

    mp4 writer

--- images/animator.py
+++ images/animator.py
@@ -1,3 +1,4 @@
+import cv2
 from PIL import Image
 import os
 from pathlib import Path
@@ -6,14 +7,11 @@ from pathlib import Path
 image_list = sorted(os.listdir(Path(__file__).parent), key=os.path.getmtime)
 reversed_images = image_list[::-1]
 all_images = image_list + reversed_images
+all_images = [image for image in all_images if "jpg" in image]
 
-images = [Image.open(file) for file in all_images if ".jpg" in file]
-
-images[0].save(
-    "mandelbrot.gif",
-    save_all=True,
-    append_images=images,
-    optimize=True,
-    duration=200,
-    loop=0
-)
+images = [cv2.imread(image) for image in all_images]
+width, height, c = cv2.imread(all_images[0]).shape
+out = cv2.VideoWriter("mandelbrot.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 5, (width, height))
+for image in images:
+    out.write(image)
+out.release()
