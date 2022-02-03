5611619112b7f8569a847ce562da11b7e977112c
brad90four <42116429+brad90four@users.noreply.github.com>
2022-02-03 15:02:05 -0500

    updates to image creator

--- mandel_2.py
+++ mandel_2.py
@@ -31,55 +31,114 @@ def mandelbrot(height, width, x=-0.5, y=0, zoom=1, max_iterations=100):
 # Default image of Mandelbrot set
 # plt.imshow(mandelbrot(800, 1000), cmap='magma')
 # plt.show()
+# interesting points:
+# x=-0.7756837699949401 y=-0.13646736999704
+# x= 0.001643721971153 y =-0.822467633298876
 
-plt.imshow(
-    mandelbrot(
-        height=4096,
-        width=4096,
-        x=0.001643721971153,
-        y=-0.822467633298876,
-        zoom=100000000,
-        max_iterations=10000
-    ),
-    cmap="magma"
-)
-ax = plt.gca()
-ax.axes.xaxis.set_visible(False)
-ax.axes.yaxis.set_visible(False)
-plt.savefig("image_wiki9.jpg", dpi=300, bbox_inches="tight", pad_inches=0, transparent=True)
+def point_test(x, y, zoom, test_num):
+    plt.imshow(
+        mandelbrot(
+            height=1024,
+            width=1024,
+            x=x,
+            y=y,
+            zoom=zoom,
+            max_iterations=1000
+        ),
+        cmap="gist_ncar"
+    )
+    ax = plt.gca()
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+    plt.savefig(f"image_test_{test_num}.jpg", dpi=300, bbox_inches="tight", pad_inches=0, transparent=True)
+    print(f"{x = }, {y = }")
 
 
 
+def pallete_test():
+    palletes = [
+        'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern', 'gnuplot',
+        'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet',
+        'turbo', 'nipy_spectral', 'gist_ncar'
+    ]
+    zoom = 10000
+    iteration = 100
+    for pallete in palletes:
+        plt.imshow(
+            mandelbrot(
+                height=1024,
+                width=1024,
+                x=0.001643721971153,
+                y=-0.822467633298876,
+                zoom=zoom,
+                max_iterations=iteration
+            ),
+            cmap=pallete
+        )
+        ax = plt.gca()
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        plt.savefig(f"image_{pallete}.jpg", dpi=300, bbox_inches="tight", pad_inches=0, transparent=True)
+        print(f"{pallete}")
 
-# for i in range(1, 50):
-#     if i == 1:
-#         zoom = 1
-#     else:
-#         zoom = int(10 ** (0.25 * i))
 
-#     if zoom < 100 and i != 1:
-#         iteration = 500
-#     elif i == 1:
-#         iteration = 100
-#     else:
-#         iteration = int(100 * i)
 
-#     plt.imshow(
-#         mandelbrot(
-#             height=1024,
-#             width=1024,
-#             x=-0.77568377,
-#             y=0.13646737,
-#             zoom=zoom,
-#             max_iterations=iteration
-#         ),
-#         cmap="prism"
-#     )
-#     ax = plt.gca()
-#     ax.axes.xaxis.set_visible(False)
-#     ax.axes.yaxis.set_visible(False)
-#     plt.savefig(f"image_{i}.jpg", dpi=1000, bbox_inches="tight", pad_inches=0, transparent=True)
-#     print(f"{i}: {zoom = }, {iteration = }")
+def anim_image():
+    for i in range(0, 51):
+        if i == 1:
+            zoom = 1
+        else:
+            zoom = int(10 ** (0.25 * i)) # every 4 iterations, zoom 10x
+
+        iteration = 100 * int(max(1, np.floor(np.log10(zoom))))
+
+        plt.imshow(
+            mandelbrot(
+                height=1024,
+                width=1024,
+                x=-0.7756837699949401,
+                y=-0.13646736999704,
+                zoom=zoom,
+                max_iterations=iteration
+            ),
+            cmap="gist_ncar"
+        )
+        ax = plt.gca()
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        plt.savefig(f"image_{i}.jpg", dpi=300, bbox_inches="tight", pad_inches=0, transparent=True)
+        print(f"{i}: {zoom = }, {iteration = }")
+
+
+
+def param_tester():
+    # optimal: dpi 300, size 1024, iteration 100-1000
+    #dpis = [100, 300, 500]
+    sizes = [(512, 512), (1024, 1024), (2048, 2048)]
+    iterations = [100, 500, 1000, 5000]
+    zooms = [1, 100, 1000, 10000]
+
+    for size in sizes:
+        dpi = 300
+        for iteration in iterations:
+            for zoom in zooms:
+                    plt.imshow(
+                        mandelbrot(
+                            height=size[0],
+                            width=size[1],
+                            x=0.001643721971153,
+                            y=-0.822467633298876,
+                            zoom=zoom,
+                            max_iterations=iteration
+                        ),
+                        cmap="nipy_spectral"
+                    )
+                    ax = plt.gca()
+                    ax.axes.xaxis.set_visible(False)
+                    ax.axes.yaxis.set_visible(False)
+                    plt.savefig(f"image_{dpi}_{zoom}_{size[0]}_{iteration}.jpg", dpi=dpi, bbox_inches="tight", pad_inches=0, transparent=True)
+                    print(f"{dpi = }, {zoom= }, {size[0] = }, {iteration = }")
+
 
 # center of spiral: -0.77568377 + 0.13646737j
 # neck: -e/7 - e/20j
@@ -96,7 +155,6 @@ plt.savefig("image_wiki9.jpg", dpi=300, bbox_inches="tight", pad_inches=0, trans
 
 # big zoom real: -1.77983107263358987710609737625462554160612202082973721464269536941106123136243079619230058357360213932827
 # big zoom imag: 0.0000768808104690890642867866831207682698968739265086118371576831945033925898525759232427462136212757994983490243570012245
-# 0.0958598997051 + 1.1501990019993j
 
 # julias
 # -0.79 + 0.15j
@@ -105,8 +163,8 @@ plt.savefig("image_wiki9.jpg", dpi=300, bbox_inches="tight", pad_inches=0, trans
 # -1.476 + 0j
 # 0.28 + 0.008j
 
-# palletes = [
-#     'flag', 'prism', 'ocean', 'gist_earth', 'terrain', 'gist_stern', 'gnuplot',
-#     'gnuplot2', 'CMRmap', 'cubehelix', 'brg', 'gist_rainbow', 'rainbow', 'jet',
-#     'turbo', 'nipy_spectral', 'gist_ncar'
-# ]
+
+# point_test(x=-0.7756837699949401, y=-0.13646736999704, zoom=1000000000000000, test_num=11)
+# pallete_test()
+# param_tester()
+anim_image()
