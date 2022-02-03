d74832e93972682388e9e7919969c3b3ab891eb2
brad90four <42116429+brad90four@users.noreply.github.com>
2022-02-03 15:18:45 -0500

    linted

--- images/animator.py
+++ images/animator.py
@@ -1,8 +1,7 @@
-import cv2
-from PIL import Image
 import os
 from pathlib import Path
 
+import cv2
 
 image_list = sorted(os.listdir(Path(__file__).parent), key=os.path.getmtime)
 reversed_images = image_list[::-1]
@@ -11,7 +10,9 @@ all_images = [image for image in all_images if "jpg" in image]
 
 images = [cv2.imread(image) for image in all_images]
 width, height, c = cv2.imread(all_images[0]).shape
-out = cv2.VideoWriter("mandelbrot.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 5, (width, height))
+out = cv2.VideoWriter(
+    "mandelbrot.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 5, (width, height)
+)
 for image in images:
     out.write(image)
 out.release()
